d3725cc696db6c88b930e70681656bb1d13b76c5
JouriDeLigt <jouri.ligt@gmail.com>
2024-03-21 16:51:39 +0100

    Confirm popup

--- admin/js/talentwave-solution-admin.js
+++ admin/js/talentwave-solution-admin.js
@@ -1,37 +1,64 @@
 (function ($) {
-    'use strict';
+  "use strict";
 
-    $(document).on('click', '.sub-content-block .secondary-button, .send-mail-bttn', function () {
-        let type = $(this).data('type');
-        let userId = $('#currentUserId').val();
-        let el = $(this);
-        let isFromContentBlock = true;
+  $(document).on("click", ".sub-content-block", function () {
+    let type = $(this).data("type");
+    $("body").addClass("popupOpen");
+    $(".twPopup .content-block-link.submit").attr("data-type", type);
 
-        if (el.hasClass('send-mail-bttn')) {
-            isFromContentBlock = false;
+    $(".twPopup").addClass("visible");
+  });
+
+  $(document).on(
+    "click",
+    ".twPopup .twPopupOverlay, .twPopup .twPopupContent .close",
+    function () {
+      $("body").removeClass("popupOpen");
+      $(".twPopup").removeClass("visible");
+    }
+  );
+
+  $(document).on(
+    "click",
+    ".twPopup .content-block-link.submit, .send-mail-bttn",
+    function () {
+      let type = $(this).data("type");
+      let userId = $("#currentUserId").val();
+      let el = $(this);
+      let isFromContentBlock = true;
+
+      if (el.hasClass("send-mail-bttn")) {
+        isFromContentBlock = false;
+      }
+
+      if (isFromContentBlock) {
+        el.parent().addClass("inactive");
+      }
+
+      $.ajax({
+        method: "POST",
+        url: "/wp-json/talentwave/get-information",
+        data: { type: type, userId: userId, key: "wNzBnWtkGF" },
+      }).done(function () {
+        alert(
+          "Bedankt voor je aanvraag, wij komen er zo spoedig mogelijk op terug!"
+        );
+        $("body").removeClass("popupOpen");
+        $(".twPopup").removeClass("visible");
+
+        if (!isFromContentBlock) {
+          el.addClass("inactive");
+          return;
         }
 
-        if (isFromContentBlock) {
-            el.parent().addClass('inactive');
-        }
-
-        $.ajax({
-            method: "POST",
-            url: "/wp-json/talentwave/get-information",
-            data: {type: type, userId: userId, key: 'wNzBnWtkGF'}
-        }).done(function () {
-            alert('Bedankt voor je aanvraag, wij komen er zo spoedig mogelijk op terug!');
-
-            if (!isFromContentBlock) {
-                el.addClass('inactive');
-                return;
-            }
-
-            el.parent().removeClass('inactive');
-            el.parent().find('p').html('<span class="success">Aanvraag is verstuurd en op goede wijze ontvangen!</span>');
-            el.parent().find('.secondary-button').remove();
-        });
-
-    })
-
+        el.parent().removeClass("inactive");
+        el.parent()
+          .find("p")
+          .html(
+            '<span class="success">Aanvraag is verstuurd en op goede wijze ontvangen!</span>'
+          );
+        el.parent().find(".secondary-button").remove();
+      });
+    }
+  );
 })(jQuery);
